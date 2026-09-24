824567cf48919363b0d237d817d50598adc67316
MisaoFT <fabiolatl@gmail.com>
2026-09-24 17:47:01 -0600

    Refactor authentication and session handling

--- pages/2_dato_lluvia_icc_13_18.py
+++ pages/2_dato_lluvia_icc_13_18.py
@@ -23,8 +23,8 @@ st.markdown("Módulo estacional para la captura y consolidación de la precipita
 URL_LOGIN = "https://redmet.icc.org.gt/login"
 URL_DATA_API = "https://redmet.icc.org.gt/redmet/comparativas"
 
-USUARIO_WS = "webservice.insivumeh@icc.org.gt"
-CONTRASENA_WS = "WebService"
+USUARIO_WS = st.secrets["ICC_USUARIO"]
+CONTRASENA_WS = st.secrets["ICC_PASS"]
 
 # --- CORRECCIÓN DE ZONA HORARIA EN EL SERVIDOR (HORA DE GUATEMALA UTC-6) ---
 tz_guatemala = ZoneInfo("America/Guatemala")
